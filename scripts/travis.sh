aabdd3fda3dacacd8515892b98f8bd9c6e6dfade
louismeunier <louismeunier123@gmail.com>
2021-01-02 17:02:33 -0500

    Initial tests

---
+++ scripts/travis.sh
@@ -0,0 +1,5 @@
+curl https://www.worldcubeassociation.org/results/misc/WCA_export.sql.zip --output wca_export
+echo "EXPORT DOWNLOADED"
+yes|unzip -d wca_unzipped wca_export 
+echo "EXPORT UNZIPPED"
+mysql -u root --password="" <  wca_unzipped/WCA_export.sql
