9b7d0af2450f03526d1ad3af07b4a84d16686347
louismeunier <louismeunier123@gmail.com>
2021-01-03 11:30:45 -0500

    Attemtping to fix error in return json, added index.html

--- scripts/sql/averageRankings.sql
+++ scripts/sql/averageRankings.sql
@@ -2,10 +2,11 @@ USE wca;
 SELECT JSON_OBJECT(
         "data",
         JSON_ARRAYAGG(JSON_OBJECT(
+                "personId",personId,
                 "eventId", eventId,
                 "best", best,
                 "worldRank", worldRank,
                 "continentRank", continentRank,
                 "countryRank", countryRank
         )) 
-) FROM RanksAverage  WHERE eventId="333" ORDER BY worldRank ;
+) FROM RanksAverage  WHERE eventId = "333" ORDER BY worldRank ;
