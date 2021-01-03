8ac875487ca6490e69f04ac33af302bbb4bc5c8f
louismeunier <louismeunier123@gmail.com>
2021-01-03 02:40:12 -0500

    Attempting to reconfigure mysql (45)

---
+++ scripts/sql/tests/rankings_test.sql
@@ -0,0 +1,11 @@
+USE wca; 
+SELECT  JSON_OBJECT(
+        "personId",personId,
+        "eventId", eventId,
+        "best", best,
+        "worldRank", worldRank,
+        "continentRank", continentRank,
+        "countryRank", countryRank
+        )
+
+FROM RanksAverage  WHERE eventId="333" AND worldRank<=10 ORDER BY worldRank ;
