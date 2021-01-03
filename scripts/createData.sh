9d1d99655a3d935bff1d31a29e49a79220a25ca1
louismeunier <louismeunier123@gmail.com>
2021-01-03 11:47:03 -0500

    Beginning to add json validation tests

--- scripts/createData.sh
+++ scripts/createData.sh
@@ -1,3 +1,6 @@
-mysql -u root --password="" < scripts/sql/averageRankings.sql > stats/rank/averageRank.json
-mysql -u root --password="" < scripts/sql/singleRankings.sql > stats/rank/singleRank.json
+#scripts/createData.sh
+
+#create ranks
+mysql -u root --password="" < scripts/sql/averageRankings.sql -ss > stats/rank/averageRank.json
+mysql -u root --password="" < scripts/sql/singleRankings.sql -ss > stats/rank/singleRank.json
 mysql -u root --password="" < scripts/sql/tests/rankings_test.sql -ss > stats/test/test.json
