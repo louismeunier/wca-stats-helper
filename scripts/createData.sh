96ba1271b76c1a360cd44ba3fe2e7e361157e4fe
louismeunier <louismeunier123@gmail.com>
2021-01-03 13:35:58 -0500

    Attempting to reconfigure file structure

--- scripts/createData.sh
+++ scripts/createData.sh
@@ -1,6 +1,7 @@
 #scripts/createData.sh
+#"demos"
+#mysql -u root --password="" < scripts/sql/tests/rankings_test.sql -ss > stats/test/test.json
 
 #create ranks
 mysql -u root --password="" < scripts/sql/averageRankings.sql -ss > stats/rank/averageRank.json
 mysql -u root --password="" < scripts/sql/singleRankings.sql -ss > stats/rank/singleRank.json
-mysql -u root --password="" < scripts/sql/tests/rankings_test.sql -ss > stats/test/test.json
--- scripts/sql/singleRankings.sql
+++ scripts/sql/singleRankings.sql
@@ -2,10 +2,10 @@ USE wca;
 SELECT JSON_OBJECT(
         "data",
         JSON_ARRAYAGG(JSON_OBJECT(
-        "eventId", eventId,
-        "best", best,
-        "worldRank", worldRank,
-        "continentRank", continentRank,
-        "countryRank", countryRank
+                "eventId", eventId,
+                "best", best,
+                "worldRank", worldRank,
+                "continentRank", continentRank,
+                "countryRank", countryRank
         )) 
-) FROM RanksAverage  WHERE eventId = "333" ORDER BY worldRank ;
+) FROM RanksSingle  WHERE eventId = "333" ORDER BY worldRank ;
