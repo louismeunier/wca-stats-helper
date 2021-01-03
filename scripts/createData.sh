a2cfa116c61947d1e99dbd69644f6b6958ed1236
louismeunier <louismeunier123@gmail.com>
2021-01-03 15:36:48 -0500

    Trying to make scripts better

--- scripts/createData.sh
+++ scripts/createData.sh
@@ -2,6 +2,7 @@
 #"demos"
 #mysql -u root --password="" < scripts/sql/tests/rankings_test.sql -ss > stats/test/test.json
 export event=333
+echo $event
 #create ranks
-mysql -u root --password="" -e "set @event='${event};'" < scripts/sql/averageRankings.sql -ss > stats/rank/averageRank.json
-mysql -u root --password="" -e "set @event='${event};'" < scripts/sql/singleRankings.sql -ss > stats/rank/singleRank.json
+mysql -u root --password="" -e "set @event='$event;'" < scripts/sql/averageRankings.sql -ss > stats/rank/averageRank.json
+mysql -u root --password="" -e "set @event='$event;'" < scripts/sql/singleRankings.sql -ss > stats/rank/singleRank.json
