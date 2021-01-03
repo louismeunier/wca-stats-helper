bb6f87c009f401f63719e8e10798f9eed431e962
louismeunier <louismeunier123@gmail.com>
2021-01-03 02:22:59 -0500

    Attempting to reconfigure mysql (42)

--- scripts/sql/rankings.sql
+++ scripts/sql/rankings.sql
@@ -1,12 +1,11 @@
 USE wca; 
-    JSON_ARRAY( 
-        JSON_OBJECT(
-            "personId",personId,
-            "eventId", eventId,
-            "best", best,
-            "worldRank", worldRank,
-            "continentRank", continentRank,
-            "countryRank", countryRank
-            )
+JSON_ARRAY( JSON_OBJECT(
+        "personId",personId,
+        "eventId", eventId,
+        "best", best,
+        "worldRank", worldRank,
+        "continentRank", continentRank,
+        "countryRank", countryRank
+        )
 )
 FROM RanksAverage  WHERE eventId="333" ORDER BY worldRank;
