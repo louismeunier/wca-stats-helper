a3a03e607f7711ebf8546a3fbb9ab352ec7f9985
louismeunier <louismeunier123@gmail.com>
2021-01-03 17:28:46 -0500

    Get all event data (attempt1)

--- scripts/createData.sh
+++ scripts/createData.sh
@@ -1,10 +1,17 @@
 #scripts/createData.sh
 #"demos"
 #mysql -u root --password="" < scripts/sql/tests/rankings_test.sql -ss > stats/test/test.json
-export event="333"
-echo $event
+
 #create ranks
-mysql -u root --password="" \
- -e "set @event='${event}'; source scripts/sql/averageRankings.sql ;" -ss > stats/rank/averageRank.json
-mysql -u root --password="" \
- -e "set @event='${event}'; source scripts/sql/singleRankings.sql ;" -ss > stats/rank/singleRank.json
+events=("222" "333" "333bld" "333fm" "333oh" "444" "555" "666" "777" "sq1" "skewb" "pyram" "minx" "clock" "444bld" "555bld")
+
+for i in "${events[@]}"
+do
+    export event=$i
+    echo $event
+    mysql -u root --password="" \
+    -e "set @event='${event}'; source scripts/sql/averageRankings.sql ;" -ss > stats/rank/averages/$i.json
+    mysql -u root --password="" \
+    -e "set @event='${event}'; source scripts/sql/singleRankings.sql ;" -ss > stats/rank/singles/$i.json
+done
+
