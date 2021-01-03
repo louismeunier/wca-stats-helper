7e215256bdde1b56a496d7535a3704bbc135a92a
louismeunier <louismeunier123@gmail.com>
2021-01-03 17:11:56 -0500

    Move metadata to be accessible

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -2,6 +2,7 @@ curl https://www.worldcubeassociation.org/results/misc/WCA_export.sql.zip --outp
 echo "EXPORT DOWNLOADED"
 yes|unzip -d wca_unzipped wca_export 
 echo "EXPORT UNZIPPED"
-mv wca_export/metadata.json stats
+mv wca_unzipped/metadata.json stats
+cp README.md stats
 mysql -u root --password="" < scripts/sql/setup.sql
 mysql -u root --password="" wca <  wca_unzipped/WCA_export.sql
