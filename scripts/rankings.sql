a52aa6dd0ebb05a3c16337c7ef1c130f31ee51bb
louismeunier <louismeunier123@gmail.com>
2021-01-02 20:01:05 -0500

    updated

--- scripts/rankings.sql
+++ scripts/rankings.sql
@@ -1,2 +1,2 @@
 USE wca;
-SELECT * FROM RanksAverage WHERE eventId="333" AND personId="2016MEUN01" OR personId="2017ZHEN10" ORDER BY RanksAverage.average;
+FROM RanksAverage SELECT *  WHERE eventId="333" AND personId="2016MEUN01" OR personId="2017ZHEN10";
