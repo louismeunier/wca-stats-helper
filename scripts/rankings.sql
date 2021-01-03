5584d4c979c1dfecc14c9d37b43245c10014adaa
louismeunier <louismeunier123@gmail.com>
2021-01-03 00:32:15 -0500

    Attempting to reconfigure mysql (25)

--- scripts/rankings.sql
+++ scripts/rankings.sql
@@ -6,4 +6,4 @@ SELECT JSON_ARRAYAGG(JSON_OBJECT(
     "continentRank",continentRank,
     "countryRank",countryRank
     )) 
-FROM RanksAverage  WHERE eventId="333" ORDER BY worldRank INTO OUTFILE "res.json";
+FROM RanksAverage  WHERE eventId="333" ORDER BY worldRank;
