ea2c63c1a8070ba380488bff0d1c20de8c00a5bd
louismeunier <louismeunier123@gmail.com>
2021-01-03 00:54:31 -0500

    Attempting to reconfigure mysql (29)

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -2,5 +2,5 @@ curl https://www.worldcubeassociation.org/results/misc/WCA_export.sql.zip --outp
 echo "EXPORT DOWNLOADED"
 yes|unzip -d wca_unzipped wca_export 
 echo "EXPORT UNZIPPED"
-mysql -u root --password="" < scripts/setup.sql
+mysql -u root --password="" < scripts/sql/setup.sql
 mysql -u root --password="" wca <  wca_unzipped/WCA_export.sql
