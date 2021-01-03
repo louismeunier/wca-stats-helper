c4ca76bd6edb533912b2aef7600bb30a5aa3d3ba
louismeunier <louismeunier123@gmail.com>
2021-01-03 17:02:45 -0500

    Move metadata to be accessible

--- scripts/createData.sh
+++ scripts/createData.sh
@@ -4,8 +4,7 @@
 export event="333"
 echo $event
 #create ranks
-pwd
-#mysql -u root --password="" < scripts/sql/tests/rankingsTest.sql -ss
 mysql -u root --password="" \
  -e "set @event='${event}'; source scripts/sql/averageRankings.sql ;" -ss > stats/rank/averageRank.json
-#mysql -u root --password=""  \-e scripts/sql/singleRankings.sql -ss > stats/rank/singleRank.json
+mysql -u root --password="" \
+ -e "set @event='${event}'; source scripts/sql/singleRankings.sql ;" -ss > stats/rank/singleRank.json
