a8637d390a92ab222e09502ea1c587d851b4fe30
louismeunier <louismeunier123@gmail.com>
2021-01-03 16:18:47 -0500

    Attempting to use variables in mysql calls

--- scripts/createData.sh
+++ scripts/createData.sh
@@ -1,10 +1,10 @@
 #scripts/createData.sh
 #"demos"
 #mysql -u root --password="" < scripts/sql/tests/rankings_test.sql -ss > stats/test/test.json
-set @event := "333"
+set event := "333"
 #create ranks
 pwd
 #mysql -u root --password="" < scripts/sql/tests/rankingsTest.sql -ss
 mysql -u root --password="" \
- -e "set @event='${event}'; source scripts/sql/averageRankings.sql ;" -ss > stats/rank/averageRank.json
+ -e "set @event=$event; source scripts/sql/averageRankings.sql ;" -ss > stats/rank/averageRank.json
 #mysql -u root --password=""  \-e scripts/sql/singleRankings.sql -ss > stats/rank/singleRank.json
