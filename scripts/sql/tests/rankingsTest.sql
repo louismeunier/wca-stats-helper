5357c273a0f60e9ede09b4d970c900cc128a828f
louismeunier <louismeunier123@gmail.com>
2021-01-05 00:34:39 -0500

    Trying to debug issue with personId

--- scripts/sql/tests/rankingsTest.sql
+++ scripts/sql/tests/rankingsTest.sql
@@ -1,2 +1,2 @@
 USE wca;
-SELECT personId FROM RanksAverage WHERE eventId="333" HAVING COUNT(personId)>1;
+SELECT personId FROM RanksAverage WHERE eventId="333" AND personId NOT_REGEXP(/\d{4}\w{4}\d{2}/);
