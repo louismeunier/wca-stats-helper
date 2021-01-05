207674e6a84fe61c45ab15a9a06692edfc543268
louismeunier <louismeunier123@gmail.com>
2021-01-05 14:30:36 -0500

    Updated travis token

--- scripts/createData.sh
+++ scripts/createData.sh
@@ -1,6 +1,7 @@
 #scripts/createData.sh
 
 #create ranks
+echo $GITHUB_TOKEN
 export MYSQL_PWD="";
 events=("222" "333" "333bf" "333fm" "333oh" "444" "555" "666" "777" "sq1" "skewb" "pyram" "minx" "clock" "444bf" "555bf")
 #mysql -u root < scripts/sql/tests/rankingsTest.sql > stats/test.json
