78e5e6e22dba40c9ef7a4976f2a1ea34915b1ed0
louismeunier <louismeunier123@gmail.com>
2021-01-05 14:10:21 -0500

    Updated travis token

--- scripts/createData.sh
+++ scripts/createData.sh
@@ -3,7 +3,7 @@
 #create ranks
 export MYSQL_PWD="";
 events=("222" "333" "333bf" "333fm" "333oh" "444" "555" "666" "777" "sq1" "skewb" "pyram" "minx" "clock" "444bf" "555bf")
-mysql -u root < scripts/sql/tests/rankingsTest.sql > stats/test.json
+#mysql -u root < scripts/sql/tests/rankingsTest.sql > stats/test.json
 for i in "${events[@]}"
 do
     export event=$i
