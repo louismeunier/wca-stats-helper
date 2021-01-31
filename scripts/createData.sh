d76a7656bf2a6487c0c83f6b21bc3a730f1d2276
louismeunier <louismeunier123@gmail.com>
2021-01-30 22:47:25 -0500

    Changed packets

--- scripts/createData.sh
+++ scripts/createData.sh
@@ -8,8 +8,8 @@ for i in "${events[@]}"
 do
     export event=$i
     echo $event
-    mysql --max_allowed_packet=20mb -u root \
+    mysql --max_allowed_packet=200m -u root \
     -e "set @event='${event}'; source scripts/sql/averageRankings.sql ;" -ss > stats/rank/averages/$i.json
-    mysql --max_allowed_packet=20mb -u root \
+    mysql --max_allowed_packet=200m -u root \
     -e "set @event='${event}'; source scripts/sql/singleRankings.sql ;" -ss > stats/rank/singles/$i.json
 done
